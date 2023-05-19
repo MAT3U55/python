a364104e2ff4c53946c13a1c9a78493711115666
MAT3U55 <123771977+MAT3U55@users.noreply.github.com>
2023-05-19 11:48:07 -0300

    Add files via upload

---
+++ python/print.py
@@ -0,0 +1,25 @@
+print("Mensagem que eu quero", " TESTE",  sep="")
+
+print(' Fulano é meu "amigo"',)
+
+print("Fulano \nÉ meu \"amigo2\"")
+
+print("1 x 1 =", 1*1)
+
+print("10² =", 10**3)
+
+print(pow (10,3))
+
+nome = "A"
+x = 1
+x= 1+1
+y = True
+
+print(nome)
+
+d1= 5.51
+d2= "5"
+print(int(d1 + int(d2)))
+print(round(d1 + int(d2)))
+
+
